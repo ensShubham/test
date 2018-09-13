0a8afb209f4d3b373c19ca53cf0cfbfa34ab5a3d
ensShubham <43240974+ensShubham@users.noreply.github.com>
2018-09-13 06:08:10 -0700

    Add files via upload

---
+++ test1/sc.js
@@ -0,0 +1,8 @@
+
+document.getElementById('submit').onclick=()=>{
+let x=document.getElementById('1').value;
+document.getElementById('2').value=x**2;
+}
+
+x=document.getElementById('myForm').elements.length;
+    console.log(x);
